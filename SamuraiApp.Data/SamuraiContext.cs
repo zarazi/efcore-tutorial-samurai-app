5f550da392f820e24a3edde437799c9d80a27d0d
zarazi <zarazi@hotmail.com>
2020-04-28 21:37:53 +0700

    Initial Repo with DB Migration

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=192.168.0.133\\EFAC;Database=SamuraiAppData;User ID=sa;Password=P@$$w0rd;");
+        }
+    }
+}
---
+++ SamuraiApp.Data/init.sql
@@ -0,0 +1,52 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Clans] (
+    [Id] int NOT NULL IDENTITY,
+    [ClanName] nvarchar(max) NULL,
+    CONSTRAINT [PK_Clans] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Samurais] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [ClanId] int NULL,
+    CONSTRAINT [PK_Samurais] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Samurais_Clans_ClanId] FOREIGN KEY ([ClanId]) REFERENCES [Clans] ([Id]) ON DELETE NO ACTION
+);
+
+GO
+
+CREATE TABLE [Quotes] (
+    [Id] int NOT NULL IDENTITY,
+    [Text] nvarchar(max) NULL,
+    [SamuraiId] int NOT NULL,
+    CONSTRAINT [PK_Quotes] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Quotes_Samurais_SamuraiId] FOREIGN KEY ([SamuraiId]) REFERENCES [Samurais] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_Quotes_SamuraiId] ON [Quotes] ([SamuraiId]);
+
+GO
+
+CREATE INDEX [IX_Samurais_ClanId] ON [Samurais] ([ClanId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200428111923_init', N'3.1.3');
+
+GO
+
